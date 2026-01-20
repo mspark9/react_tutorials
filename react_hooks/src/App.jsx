107821e21cc0bd6bc80408687be74a6827ac5147
mspark9 <stellaaa0906@gmail.com>
2026-01-20 19:36:59 +0900

    react_hooks

---
+++ react_hooks/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import UseRef from './components/UseRef'
+
+const App = () => {
+
+  return (
+    <div>
+      <UseRef />
+    </div>
+  )
+}
+
+export default App
---
+++ react_hooks/src/components/UseRef.jsx
@@ -0,0 +1,28 @@
+// const ref = useRef(value) -> {current: value}
+// state와 마찬가지로 특정 값을 저장하고 유지할 때 사용 : hook은 함수이고, 이는 매번 렌더링 될 때마다 재호출하게 된다. 이때 상태 변화 없이 값만 유지하고 싶을 때 사용한다.
+
+import { useRef, useState } from "react"
+
+const UseRef = () => {
+    const [count, setCount] = useState(0);
+    const countRef = useRef(0);
+    // console.log(countRef.current);
+
+    const increaseState = () => {
+        setCount(count + 1);
+    }
+    const increaseRef = () => {
+        countRef.current += 1;
+    }
+
+  return (
+    <div>
+        <p>state : {count}</p>
+        <p>Ref : {countRef.current}</p>
+        <button onClick={increaseState}>state up</button><br />
+        <button onClick={increaseRef}>Ref up</button>
+    </div>
+  )
+}
+
+export default UseRef
