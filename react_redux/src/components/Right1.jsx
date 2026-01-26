0c647ad6ed67eb7abe8d1ad3588c39cbb72b7390
mspark9 <stellaaa0906@gmail.com>
2026-01-26 10:12:31 +0900

    react redux

--- react_redux/src/components/Right1.jsx
+++ react_redux/src/components/Right1.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Right2 from './Right2'
 
-const Right1 = (props) => {
+const Right1 = () => {
     return (
         <div>
             <h1>Right1</h1>
-            <Right2 number={props.number} onIncrease={() => props.onIncrease()} />
+            <Right2 />
         </div>
     )
 }
